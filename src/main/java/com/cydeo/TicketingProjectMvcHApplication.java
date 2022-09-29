052e79c1aa0fc82f56289c8624fe015b495a8ee6
Hrvoje <hbrkljacic@gmail.com>
2022-09-29 17:41:56 +0200

    newProject

---
+++ src/main/java/com/cydeo/TicketingProjectMvcHApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectMvcHApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectMvcHApplication.class, args);
+    }
+
+}
