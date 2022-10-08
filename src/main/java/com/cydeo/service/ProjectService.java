aa99c7d1f6e4397c90e5851bf3683854390f474b
Hrvoje <hbrkljacic@gmail.com>
2022-10-07 21:45:23 -0400

    part3projectCreate

---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,7 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+
+                                                            //String - unique object - Project Code
+public interface ProjectService extends CrudService<ProjectDTO,String>{
+}
