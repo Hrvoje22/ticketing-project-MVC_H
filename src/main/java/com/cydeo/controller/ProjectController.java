6bad4da13e68e01fdc264fc8a75e9da0b7f23c0a
Hrvoje <hbrkljacic@gmail.com>
2022-10-07 22:23:33 -0400

    part3projectCreate

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,11 +1,14 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -28,6 +31,16 @@ public class ProjectController {
         model.addAttribute("projects",projectService.findAll());
 
         return "/project/create";
+    }
+
+    @PostMapping("create")
+    public String insertProject(@ModelAttribute("project") ProjectDTO project){
+
+        project.setProjectStatus(Status.OPEN);
+
+        projectService.save(project);
+
+        return "redirect:/project/create";
     }
 
 
